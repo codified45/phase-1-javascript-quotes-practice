7992202319cf7134f8a6b3346278135c38035c06
Jack Lach <jack.lach4@gmail.com>
2023-03-14 05:51:43 -0400

    implement sortByAuthorViaFetch()

--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@
 const currentQuotesUrl = "http://localhost:3000/quotes";
 const currentLikesUrl = "http://localhost:3000/likes";
 const currentQuotesAndLikesUrl = "http://localhost:3000/quotes?_embed=likes";
+const sortByAuthorUrl = "http://localhost:3000/quotes?_sort=author";
+const getQuoteLikesUrl = "http://localhost:3000/likes?quoteId=";
 
 document.addEventListener("DOMContentLoaded", function() {
     const quoteListContainer = document.getElementById('quote-list');
     const newQuoteForm = document.getElementById('new-quote-form');
     newQuoteForm.addEventListener('submit', submitNewQuote);
+    const sortBtn = document.getElementById('srtBtn');
+    // sortBtn.addEventListener('click', sortByAuthorViaJs);
+    sortBtn.addEventListener('click', sortByAuthorViaFetch);
 
     populatePageWithQuotesAndLikes();
 
@@ -216,11 +221,86 @@ document.addEventListener("DOMContentLoaded", function() {
         textSubmit.type = "submit";
         form.append(textInput, textSubmit);
         form.className = "edit-form-hidden";
-        form.addEventListener('submit', editQuote); // can also change to click, but I think thats bad practice (hitting enter to submit should work too)
+        form.addEventListener('submit', editQuote);
         bquote.append(p, footer, br, likeBtn, dltBtn, editBtn, form);
         li.append(bquote);
         return li;
     };
+
+    function changeLikes(quoteId, likeBtn) {
+        let quoteLikeCheckUrl = getQuoteLikesUrl + quoteId;
+        console.log(quoteLikeCheckUrl);
+        fetch(quoteLikeCheckUrl)
+        .then(res => res.json())
+        .then(arr => {
+            console.log(arr.length);
+            numberOfLikes = arr.length;
+            likeBtn.textContent = `Likes: ${numberOfLikes}`;
+        });
+    };
+
+    // function changeLikes(likeBtn, numberOfLikes) {
+    //     likeBtn.textContent = `Likes: ${numberOfLikes}`
+    // };
+
+    // function buildDomAfterAuthorSort(quoteObj) {
+    //     let id = quoteObj.id;
+
+    //     let quoteLikeCheckUrl = getQuoteLikesUrl + id;
+    //     console.log(quoteLikeCheckUrl);
+
+    //     fetch(quoteLikeCheckUrl)
+    //     .then(res => res.json())
+    //     .then(arr => {
+    //         console.log(arr.length);
+    //         let numberOfLikes = arr.length;
+        
+    //         console.log(id);
+    //         console.log(numberOfLikes);
+    //         let li = document.createElement('li');
+    //         li.classList.add('quote-card');
+    //         let bquote = document.createElement('blockquote');
+    //         bquote.classList.add('blockquote');
+    //         bquote.id = id;
+    //         let p = document.createElement('p');
+    //         p.classList.add('mb-0');
+    //         p.textContent = quoteObj.quote;
+    //         let footer = document.createElement('footer');
+    //         footer.classList.add('blockquote-footer');
+    //         footer.textContent = quoteObj.author;
+    //         let br = document.createElement('br');
+    //         let likeBtn = document.createElement('button');
+    //         likeBtn.classList.add('btn-success');
+    //         likeBtn.id = `likeBtn${id}`;
+    //         likeBtn.addEventListener('click', newLike);
+    //         let dltBtn = document.createElement('button');
+    //         dltBtn.classList.add('btn-danger');
+    //         dltBtn.textContent = "Delete";
+    //         dltBtn.id = `dltBtn${id}`;
+    //         dltBtn.addEventListener('click', dltQuote);
+    //         let editBtn = document.createElement('button');
+    //         editBtn.classList.add('btn-edit');
+    //         editBtn.textContent = "Edit Quote";
+    //         editBtn.id = `editBtn${id}`;
+    //         editBtn.addEventListener('click', showForm);
+    //         let form = document.createElement('form');
+    //         form.id = id;
+    //         let textInput = document.createElement('input');
+    //         textInput.type = "text";
+    //         textInput.className = 'text-input';
+    //         let textSubmit = document.createElement('input');
+    //         textSubmit.type = "submit";
+    //         form.append(textInput, textSubmit);
+    //         form.className = "edit-form-hidden";
+    //         form.addEventListener('submit', editQuote);
+    //         likeBtn.textContent = `Likes: ${numberOfLikes}`;
+    //         bquote.append(p, footer, br, likeBtn, dltBtn, editBtn, form);
+    //         li.append(bquote);
+    //         console.log(numberOfLikes);
+    //         return li;
+    //     });
+    // 
+    // };
 
     function dltQuote(e) {
         let btnIdNum = e.target.parentNode.id;
@@ -236,6 +316,52 @@ document.addEventListener("DOMContentLoaded", function() {
         let bquote = document.getElementById(btnIdNum);
         let dltLi = bquote.parentNode;
         dltLi.remove();
+    };
+
+    function sortByAuthorViaJs(e) {
+        console.log(e.target.textContent);
+        console.log(e.target);
+        console.log(e.target.value);
+        if (e.target.value === "off") {
+            e.target.value = "on";
+            e.target.textContent = "Sort Quotes: On";
+            
+        } else if (e.target.value === "on") {
+            e.target.value = "off";
+            e.target.textContent = "Sort Quotes: Off"
+            quoteListContainer.replaceChildren();
+            populatePageWithQuotesAndLikes();
+        };
+        console.log(e.target.value);
+
+    };
+
+    function sortByAuthorViaFetch(e) {
+        console.log(e.target.textContent);
+        console.log(e.target);
+        console.log(e.target.value);
+        quoteListContainer.replaceChildren();
+        if (e.target.value === "off") {
+            e.target.value = "on";
+            e.target.textContent = "Sort Quotes: On";
+            fetch(sortByAuthorUrl)
+            .then(res => res.json())
+            .then(arr => {
+                for (const obj of arr) {
+                    console.log(obj);
+                    console.log(obj.id);
+                    quoteListContainer.append(buildSingleNewQuoteDom(obj));
+                    let likeBtn = document.getElementById(`likeBtn${obj.id}`);
+                    console.log(likeBtn);
+                    changeLikes(obj.id, likeBtn);
+                };
+            });
+        } else if (e.target.value === "on") {
+            e.target.value = "off";
+            e.target.textContent = "Sort Quotes: Off";
+            populatePageWithQuotesAndLikes();
+        };
+        console.log(e.target.value);
     };
 
 });
@@ -258,23 +384,30 @@ pessimistic rendering is recommended.
         <button class='btn-danger'>Delete</button>
       </blockquote>
     </li>
-3. () Submitting the form creates a new quote and adds it to the list of quotes without having to refresh the page. Pessimistic rendering is recommended.
+3. (done) Submitting the form creates a new quote and adds it to the list of quotes without having to refresh the page. Pessimistic rendering is recommended.
 
-Clicking the delete button should delete the respective quote from the API and remove it from the page without having to refresh.
+4. (done) Clicking the delete button should delete the respective quote from the API and remove it from the page without having to refresh.
 
-Clicking the like button will create a like for this particular quote in the API and update the number of likes displayed on the page without having to refresh.
+5. (done) Clicking the like button will create a like for this particular quote in the API and update the number of likes displayed on the page without having to refresh.
 
-Use a POST request to http://localhost:3000/likes
+6. (done) Use a POST request to http://localhost:3000/likes
 The body of the request should be a JSON object containing a key of quoteId, with an integer value. Use the ID of the quote you're creating the like for — e.g. { quoteId: 5 } to create a like for quote 5.
-IMPORTANT: if the quoteID is a string for some reason (for example, if you've pulled the ID from a dataset) the index page will not include the like you create on any quote.
-Bonus (not required): add a createdAt key to your object to track when the like was created. Use UNIX timeLinks to an external site. (the number of seconds since January 1, 1970). The documentationLinks to an external site. for the JS Date class may be helpful here!
+7. (done) IMPORTANT: if the quoteID is a string for some reason (for example, if you've pulled the ID from a dataset) the index page will not include the like you create on any quote.
+
+8. (done) Bonus (not required): add a createdAt key to your object to track when the like was created. Use UNIX timeLinks to an external site. (the number of seconds since January 1, 1970). The documentationLinks to an external site. for the JS Date class may be helpful here!
+
 Extend Your Learning
-Add an edit button to each quote-card that will allow the editing of a quote. (Hint: there is no 'correct' way to do this. You can try creating a hidden form that will only show up when hitting the edit button.)
-Currently, the number of likes of each post does not persist on the frontend after we refresh, as we set the beginning value to 0. Include an additional fetch to always have an updated number of likes for each post. You will send a GET request to http://localhost:3000/likes?quoteId= and interpolate the id of a given post.
-Add a sort button that can be toggled on or off. When off the list of quotes will appear sorted by the ID. When the sort is active, it will display the quotes by author's name, alphabetically.
+9. (done) Add an edit button to each quote-card that will allow the editing of a quote. (Hint: there is no 'correct' way to do this. You can try creating a hidden form that will only show up when hitting the edit button.)
+
+10. (done) Currently, the number of likes of each post does not persist on the frontend after we refresh, as we set the beginning value to 0. Include an additional fetch to always have an updated number of likes for each post. You will send a GET request to http://localhost:3000/likes?quoteId= and interpolate the id of a given post. (did it differently)
+
+11. Add a sort button that can be toggled on or off. When off the list of quotes will appear sorted by the ID. When the sort is active, it will display the quotes by author's name, alphabetically.
+
 One way of doing this is to sort the quotes in JS after you've retrieved them from the API. Try this way first.
-Another way of doing this is to make a fetch to http://localhost:3000/quotes?_sort=author
+
+12. Another way of doing this is to make a fetch to http://localhost:3000/quotes?_sort=author
 What are the pros and cons in doing the sorting on the client vs. the server? Discuss with a partner.
+
 Conclusion
 Building an application like this is a typical interview exercise. It's not uncommon to be set in front of a foreign computer (or asked to bring your own) and to receive a specification like this.
 
